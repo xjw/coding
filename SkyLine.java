9131112ab5b5c27afdf8773c2178efb236c0d814
XJ Wang <xwang@netflix.com>
2013-11-19 16:32:03 -0800

    add skyline

---
+++ SkyLine.java
@@ -0,0 +1,16 @@
+/*
+    S1: {[2,5], vol=10}, {[6,10], vol=2}, ...
+    S2: {[1,6], vol=1}, {[8,12], vol=8}, ...
+    ...
+
+    Find largest vol from each segmentation and print it out
+    [1,2],vol=1, [2,5], vol=10, [5,6], vol = 1, [6,8], vol = 2, [8,12], vol = 8.
+
+    Sources:
+    http://www.mitbbs.com/article_t1/JobHunting/32579865_0_1.html
+    http://www.mitbbs.com/article_t1/JobHunting/32569901_0_2.html
+*/
+public class SkyLine {
+    public static void Main(String args) {
+    }
+}
